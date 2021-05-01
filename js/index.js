c163aa9b282258cc063534c9c61284086caf57cb
Cortlynd101 <CortlyndCox@gmail.com>
2021-05-01 13:58:43 -0600

    Changed the funcs a little.

--- js/index.js
+++ js/index.js
@@ -1,110 +1,118 @@
-function Flavors() {
-    let vanilla = document.getElementById('vanilla');
-    if (vanilla.checked == true) {
-        alert('yeehaw');
-    }
-    let chocolate = document.getElementById('chocolate');
-    if (chocolate.checked == true) {
-        alert('yeehaw');
-    }
-    let strawberry = document.getElementById('strawberry');
-    if (strawberry.checked == true) {
-        alert('yeehaw');
-    }
-    let mint = document.getElementById('mint');
-    if (mint.checked == true) {
-        alert('yeehaw');
-    }
-    let mango = document.getElementById('mango');
-    if (mango.checked == true) {
-        alert('yeehaw');
-    }
-    let cotton_candy = document.getElementById('cotton-candy');
-    if (cotton_candy.checked == true) {
-        alert('yeehaw');
-    }
-}
+function RunFuncs() {
 
-function Syrups () {
-    let caramel_syrup = document.getElementById('caramel-syrup');
-    if (caramel_syrup.checked == true) {
-        alert('yeehaw');
+    function Flavors() {
+        let vanilla = document.getElementById('vanilla');
+        if (vanilla.checked == true) {
+            alert('yeehaw');
+        }
+        let chocolate = document.getElementById('chocolate');
+        if (chocolate.checked == true) {
+            alert('yeehaw');
+        }
+        let strawberry = document.getElementById('strawberry');
+        if (strawberry.checked == true) {
+            alert('yeehaw');
+        }
+        let mint = document.getElementById('mint');
+        if (mint.checked == true) {
+            alert('yeehaw');
+        }
+        let mango = document.getElementById('mango');
+        if (mango.checked == true) {
+            alert('yeehaw');
+        }
+        let cotton_candy = document.getElementById('cotton-candy');
+        if (cotton_candy.checked == true) {
+            alert('yeehaw');
+        }
     }
-    let chocolate_syrup = document.getElementById('chocolate-syrup');
-    if (chocolate_syrup.checked == true) {
-        alert('yeehaw');
-    }
-    let strawberry_syrup = document.getElementById('strawberry-syrup');
-    if (strawberry_syrup.checked == true) {
-        alert('yeehaw');
-    }
-}
 
-function Soft_Toppings () {
-    let bananas = document.getElementById('bananas');
-    if (bananas.checked == true) {
-        alert('yeehaw');
+    function Syrups() {
+        let caramel_syrup = document.getElementById('caramel-syrup');
+        if (caramel_syrup.checked == true) {
+            alert('yeehaw');
+        }
+        let chocolate_syrup = document.getElementById('chocolate-syrup');
+        if (chocolate_syrup.checked == true) {
+            alert('yeehaw');
+        }
+        let strawberry_syrup = document.getElementById('strawberry-syrup');
+        if (strawberry_syrup.checked == true) {
+            alert('yeehaw');
+        }
     }
-    let blueberries = document.getElementById('blueberries');
-    if (blueberries.checked == true) {
-        alert('yeehaw');
-    }
-    let brownie_bites = document.getElementById('brownie-bites');
-    if (brownie_bites.checked == true) {
-        alert('yeehaw');
-    }
-    let cookie_dough = document.getElementById('cookie-dough');
-    if (cookie_dough.checked == true) {
-        alert('yeehaw');
-    }
-    let strawberries = document.getElementById('strawberries');
-    if (strawberries.checked == true) {
-        alert('yeehaw');
-    }
-    let gummy_bears = document.getElementById('gummy-bears');
-    if (gummy_bears.checked == true) {
-        alert('yeehaw');
-    }
-}
 
-function Hard_Toppings () {
-    let mint_chips = document.getElementById('mint-chips');
-    if (mint_chips.checked == true) {
-        alert('yeehaw');
+    function Soft_Toppings() {
+        let bananas = document.getElementById('bananas');
+        if (bananas.checked == true) {
+            alert('yeehaw');
+        }
+        let blueberries = document.getElementById('blueberries');
+        if (blueberries.checked == true) {
+            alert('yeehaw');
+        }
+        let brownie_bites = document.getElementById('brownie-bites');
+        if (brownie_bites.checked == true) {
+            alert('yeehaw');
+        }
+        let cookie_dough = document.getElementById('cookie-dough');
+        if (cookie_dough.checked == true) {
+            alert('yeehaw');
+        }
+        let strawberries = document.getElementById('strawberries');
+        if (strawberries.checked == true) {
+            alert('yeehaw');
+        }
+        let gummy_bears = document.getElementById('gummy-bears');
+        if (gummy_bears.checked == true) {
+            alert('yeehaw');
+        }
     }
-    let chocolate_chips = document.getElementById('chocolate-chips');
-    if (chocolate_chips.checked == true) {
-        alert('yeehaw');
-    }
-    let coconut_shavings = document.getElementById('coconut-shavings');
-    if (coconut_shavings.checked == true) {
-        alert('yeehaw');
-    }
-    let mANDm = document.getElementById('mANDm');
-    if (mANDm.checked == true) {
-        alert('yeehaw');
-    }
-    let oreo_pieces = document.getElementById('oreo-pieces');
-    if (oreo_pieces.checked == true) {
-        alert('yeehaw');
-    }
-    let peanuts = document.getElementById('peanuts');
-    if (peanuts.checked == true) {
-        alert('yeehaw');
-    }
-}
 
-function Extras () {
-    let pirouline = document.getElementById('pirouline');
-    if (pirouline.checked == true) {
-        alert('yeehaw');
+    function Hard_Toppings() {
+        let mint_chips = document.getElementById('mint-chips');
+        if (mint_chips.checked == true) {
+            alert('yeehaw');
+        }
+        let chocolate_chips = document.getElementById('chocolate-chips');
+        if (chocolate_chips.checked == true) {
+            alert('yeehaw');
+        }
+        let coconut_shavings = document.getElementById('coconut-shavings');
+        if (coconut_shavings.checked == true) {
+            alert('yeehaw');
+        }
+        let mANDm = document.getElementById('mANDm');
+        if (mANDm.checked == true) {
+            alert('yeehaw');
+        }
+        let oreo_pieces = document.getElementById('oreo-pieces');
+        if (oreo_pieces.checked == true) {
+            alert('yeehaw');
+        }
+        let peanuts = document.getElementById('peanuts');
+        if (peanuts.checked == true) {
+            alert('yeehaw');
+        }
     }
-    let whipped_cream = document.getElementById('whipped-cream');
-    if (whipped_cream.checked == true) {
-        alert('yeehaw');
+
+    function Extras() {
+        let pirouline = document.getElementById('pirouline');
+        if (pirouline.checked == true) {
+            alert('yeehaw');
+        }
+        let whipped_cream = document.getElementById('whipped-cream');
+        if (whipped_cream.checked == true) {
+            alert('yeehaw');
+        }
+        let waffle_cone = document.getElementById('waffle-cone');
+        if (waffle_cone.checked == true) {
+            alert('yeehaw');
+        }
     }
-    let waffle_cone = document.getElementById('waffle-cone');
-    if (waffle_cone.checked == true) {
-        alert('yeehaw');
-    }
+    Flavors();
+    Syrups();
+    Soft_Toppings();
+    Hard_Toppings();
+    Extras();
 }
